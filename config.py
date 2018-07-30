f2715145af309709d0ef471859d2345a2e13e609
Jesus Montoya <ec2-user@ip-172-31-17-118.us-east-2.compute.internal>
2018-07-30 23:00:41 +0000

    First commit

---
+++ config.py
@@ -0,0 +1,16 @@
+# Firebase data
+firebaseConf = {
+    "apiKey": "",
+    "authDomain": "",
+    "databaseURL": "",
+    "storageBucket": ""
+  }
+
+
+# Twilio data
+# Your Account SID from twilio.com/console
+account_sid = ""
+# Your Auth Token from twilio.com/console
+auth_token = ""
+
+
