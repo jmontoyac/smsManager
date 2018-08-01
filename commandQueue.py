28f5529b442ae70c1564f8f8f1a218020a81e329
jmontoyac <jmontoyac@gmail.com>
2018-07-31 23:49:04 -0500

    Queue and dequeue ok, added class message to store messages

--- commandQueue.py
+++ commandQueue.py
@@ -6,26 +6,42 @@ from time import sleep
 
 command_queue = Queue.Queue()
 
+# Class to hold a message
+class message:
+	destination = ""
+	contents = ""
+	status = "INITIAL"
+
 # This is the worker for threading
 def send_mesage():
 	while not command_queue.empty():	
 	    # Get message from queue
-	    print "Getting message from queue: " + str(command_queue.get())
+	    msg = command_queue.get()
+	    print "Getting message from queue"
+	    print "Destination: " + msg.destination
+	    print "Message: " + msg.contents
+	    print "Status: " + msg.status
 	    print "Sending message"
-	    print "Timeout seconds: " + str(config.SMS_RESPONSE_TIMEOUT)
-	    sleep(config.SMS_RESPONSE_TIMEOUT)
+	    print "Wait for response timeout in seconds: " + str(config.SMS_RESPONSE_TIMEOUT)
+	    print "-----------------------------------------------"
+	    for x in range(config.SMS_RETRIES):
+	    	print "Message to " + msg.destination + " attempt: " + str(x+1) + " out of " + str(config.SMS_RETRIES)
+	    	sleep(config.SMS_RESPONSE_TIMEOUT)
 	print "Sent all messages from queue"
 
 
 def main(argv):
 	print "Add all arguments to command queue"
-	for arg in argv:
-		command_queue.put(arg)
-		print "Argument added to queue: " + str(arg)
+	print "argv length: " + str(len(argv))
+	x=0
+	while x< len(argv):
+		m = message()
+		m.destination = argv[x]
+		m.contents = argv[x+1]
+		command_queue.put(m)
+		x+=2
 	sleep(3)
 	send_mesage()
-
-
 
 if __name__ == "__main__":
 	main(sys.argv[1:])
